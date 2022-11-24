d8f4617c14b3ed31320d17e7fd5c02311eefced3
Christoph Ungricht <ch.ungricht@scewo.ch>
2022-11-24 16:27:17 +0100

    refactor!: Make modules PEP-8 compliant.
    Signed-off-by: Christoph Ungricht <ch.ungricht@scewo.ch>

--- jira_access/basic_auth.py
+++ jira_access/basic_auth.py
@@ -1,16 +1,15 @@
-## Class to be used as auth mechanism for the requests module
-##
 import base64
 
 import requests
 
 
 class BasicAuth(requests.auth.AuthBase):
+    """Class to be used as auth mechanism for the requests module."""
 
     def __init__(self, mail, token):
-        accessKey = f"{mail}:{token}"
-        self._accessKeyEncoded = base64.b64encode(accessKey.encode("ascii")).decode('ascii')
+        access_key = f"{mail}:{token}"
+        self._access_key_encoded = base64.b64encode(access_key.encode("ascii")).decode("ascii")
 
     def __call__(self, r):
-        r.headers["authorization"] = "Basic " + self._accessKeyEncoded
+        r.headers["authorization"] = "Basic " + self._access_key_encoded
         return r

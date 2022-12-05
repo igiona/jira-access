44e6c6a38e9b7d357357ee8b44130da6a3a06026
Christoph Ungricht <ch.ungricht@scewo.ch>
2022-12-05 17:16:17 +0100

    refactor: Added type annotations to authenticators.
    Signed-off-by: Christoph Ungricht <ch.ungricht@scewo.ch>

--- jira_access/basic_auth.py
+++ jira_access/basic_auth.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import base64
 
 import requests
@@ -6,10 +8,10 @@ import requests
 class BasicAuth(requests.auth.AuthBase):
     """Class to be used as auth mechanism for the requests module."""
 
-    def __init__(self, mail, token):
+    def __init__(self, mail: str, token: str):
         access_key = f"{mail}:{token}"
         self._access_key_encoded = base64.b64encode(access_key.encode("ascii")).decode("ascii")
 
-    def __call__(self, r):
+    def __call__(self, r: requests.PreparedRequest) -> requests.PreparedRequest:
         r.headers["authorization"] = "Basic " + self._access_key_encoded
         return r

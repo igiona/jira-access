1146989464cdeb8107ce8aeb7e1557edd7f0e270
Christoph Ungricht <ch.ungricht@scewo.ch>
2022-12-05 17:16:17 +0100

    refactor: Added type annotations to authenticators.
    Signed-off-by: Christoph Ungricht <ch.ungricht@scewo.ch>

--- jira_access/bearer_auth.py
+++ jira_access/bearer_auth.py
@@ -1,12 +1,14 @@
+from __future__ import annotations
+
 import requests
 
 
 class BearerAuth(requests.auth.AuthBase):
     """Class to be used as auth mechanism for the requests module."""
 
-    def __init__(self, token):
+    def __init__(self, token: str):
         self.token = token
 
-    def __call__(self, r):
+    def __call__(self, r: requests.PreparedRequest) -> requests.PreparedRequest:
         r.headers["authorization"] = "Bearer " + self.token
         return r

5d3efa33c42df5f7bd972e529a61d0eb61219779
Christoph Ungricht <ch.ungricht@scewo.ch>
2022-12-12 13:17:20 +0100

    test: Moved email and base url to env variable.
    Signed-off-by: Christoph Ungricht <ch.ungricht@scewo.ch>

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -4,16 +4,17 @@ from typing import Optional
 
 from jira_access.jira_query_executor import JiraQueryExecutor
 
-EMAIL: str = "ch.ungricht@scewo.ch"
-JIRA_BASE_URL = "https://scewosw.atlassian.net/"
-
 
 def test_connection():
     jira_api_token: Optional[str] = os.getenv("JIRA_API_TOKEN")
+    jira_email: Optional[str] = os.getenv("JIRA_EMAIL")
+    jira_base_url: Optional[str] = os.getenv("JIRA_BASE_URL")
 
-    assert jira_api_token is not None, '"JIRA_API_TOKE" not set as en environment variable.'
+    assert jira_api_token is not None, '"JIRA_API_TOKE" not set as an environment variable.'
+    assert jira_email is not None, '"JIRA_EMAIl" not set as an environment variable.'
+    assert jira_base_url is not None, '"JIRA_BASE_URL" not set as an environment variable'
 
-    jira_query_exec = JiraQueryExecutor.from_mail_and_token(EMAIL, jira_api_token, JIRA_BASE_URL)
+    jira_query_exec = JiraQueryExecutor.from_mail_and_token(jira_email, jira_api_token, jira_base_url)
 
     jql_query = 'project = "TES" AND assignee = currentuser()'
     res = jira_query_exec.execute_jql_query(jql_query)
